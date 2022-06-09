a7d94bdb5f090eccac0d7e5862b3a97ea8eb14b4
mansamusah254 <mansamusah830@gmail.com>
2022-06-09 11:19:55 +0300

    API calling

--- app/src/main/java/com/newsapp/skynews/RequestManager.java
+++ app/src/main/java/com/newsapp/skynews/RequestManager.java
@@ -2,8 +2,12 @@ package com.newsapp.skynews;
 
 import android.content.Context;
 
+import com.newsapp.skynews.Models.NewsApiResponse;
+
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Query;
 
 public class RequestManager {
     Context context;
@@ -12,4 +16,17 @@ public class RequestManager {
             .baseUrl("https://newsapi.org/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
+
+    public RequestManager(Context context) {
+        this.context = context;
+    }
+
+    public interface CallNewsApi {
+        Call<NewsApiResponse> callHeadlines(
+                @Query("country") String country,
+                @Query("category") String category,
+                @Query("q") String query,
+                @Query("apiKey") String api_key
+        );
+    }
 }
