4e63888b9636a1ba4bb23dadfcbd6774d63ca6b3
mansamusah254 <mansamusah830@gmail.com>
2022-06-09 11:12:16 +0300

    request manager

---
+++ app/src/main/java/com/newsapp/skynews/RequestManager.java
@@ -0,0 +1,15 @@
+package com.newsapp.skynews;
+
+import android.content.Context;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RequestManager {
+    Context context;
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
