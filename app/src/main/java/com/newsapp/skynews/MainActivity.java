2af352abe91c911edf32112e297dbd10c78a52c5
mansamusah254 <mansamusah830@gmail.com>
2022-06-09 13:58:37 +0300

    final commit

--- app/src/main/java/com/newsapp/skynews/MainActivity.java
+++ app/src/main/java/com/newsapp/skynews/MainActivity.java
@@ -1,6 +1,7 @@
 package com.newsapp.skynews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.newsapp.skynews.Models.NewsApiResponse;
 import com.newsapp.skynews.Models.NewsHeadlines;
@@ -21,11 +23,29 @@ public class MainActivity extends AppCompatActivity implements SelectListener, V
     CustomAdapter adapter;
     ProgressDialog dialog;
     Button b1,b2,b3,b4,b5,b6,b7;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                dialog.setTitle("Fetching article" + query);
+                dialog.show();
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getNewsHeadlines(listener, "general",query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         dialog = new ProgressDialog(this);
         dialog.setTitle("Getting News....for you!");
@@ -59,13 +79,18 @@ public class MainActivity extends AppCompatActivity implements SelectListener, V
     private final OnFetchDataListener<NewsApiResponse> listener = new OnFetchDataListener<NewsApiResponse>() {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
-            showNews(list);
-            dialog.dismiss();
+            if (list.isEmpty()){
+                Toast.makeText(MainActivity.this, "No data Found", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                showNews(list);
+                dialog.dismiss();
+            }
         }
 
         @Override
         public void onError(String message) {
-
+            Toast.makeText(MainActivity.this, "An Error Occurred!", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -88,6 +113,7 @@ public class MainActivity extends AppCompatActivity implements SelectListener, V
         Button button = (Button) v;
         String category = button.getText().toString();
         dialog.setTitle("Fetching" + category);
+        dialog.show();
         RequestManager manager = new RequestManager(this);
         manager.getNewsHeadlines(listener, category,null);
     }
