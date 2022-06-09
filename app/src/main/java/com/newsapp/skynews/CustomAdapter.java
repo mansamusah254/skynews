ea89dcf88d512daee742941cd00fc171685de48c
mansamusah254 <mansamusah830@gmail.com>
2022-06-09 10:51:46 +0300

    constructor

--- app/src/main/java/com/newsapp/skynews/CustomAdapter.java
+++ app/src/main/java/com/newsapp/skynews/CustomAdapter.java
@@ -1,4 +1,37 @@
 package com.newsapp.skynews;
 
-public class CustomAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.newsapp.skynews.Models.NewsHeadlines;
+
+import java.util.List;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
+    private Context context;
+    private List<NewsHeadlines> headlines;
+
+    public CustomAdapter(Context context, List<NewsHeadlines> headlines) {
+        this.context = context;
+        this.headlines = headlines;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
