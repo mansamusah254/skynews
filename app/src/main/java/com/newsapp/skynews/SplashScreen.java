faee8d8b373d967de829520b11b9f7287b9bc92d
mansamusah254 <mansamusah830@gmail.com>
2022-06-21 16:15:24 +0300

    splash screen

---
+++ app/src/main/java/com/newsapp/skynews/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.newsapp.skynews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView appName;
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        appName = findViewById(R.id.appName);
+        lottie = findViewById(R.id.lottie);
+
+        appName.animate().translationX(-1400).setDuration(2700).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        },5000);
+    }
+}
